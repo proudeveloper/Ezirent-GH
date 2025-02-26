c44d05116d95cec7e3d80ad132ce2906eccd6c6d
prouddeveloper <141268731+proudeveloper@users.noreply.github.com>
2025-02-26 13:05:48 +0000

    Delete public directory

--- public/tenants/assets/js/layout.js
+++
@@ -1,4 +0,0 @@
-! function() {
-    "use strict";
-    "dark-mode-switch" == sessionStorage.getItem("is_visited") ? document.documentElement.setAttribute("data-bs-theme", "dark") : "light-mode-switch" != sessionStorage.getItem("is_visited") && "rtl-mode-switch" != sessionStorage.getItem("is_visited") || document.documentElement.setAttribute("data-bs-theme", "light")
-}();
